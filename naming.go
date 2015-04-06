5e4f721a654cda9d427350d959794a4505229935
Evgenii Shevchenko <shvgn@protonmail.ch>
2015-04-06 22:15:27 +0300

    naming functionality and its test

--- naming.go
+++ naming.go
@@ -7,7 +7,42 @@
 
 package main
 
-func addPreSuffix(sfx string) string {
-	notImplemented()
-	return ""
+import (
+	"path/filepath"
+	"strings"
+)
+
+// Now spectool knows only nanometers and electron-volts
+func isAnyKnownSuffix(s string) bool {
+	knownSuffixes := []string{".nm", ".ev"}
+	for _, sfx := range knownSuffixes {
+		if s == sfx {
+			return true
+		}
+	}
+	return false
+}
+
+// Adds pre-suffix or extension to filename or changes the existing one
+// addPreSuffix("data1.txt", "ev") == data1.ev.txt
+// addPreSuffix("data1.nm", "ev") == data1.ev
+// addPreSuffix("data1.nm.txt", "ev") == data1.ev.txt
+func addPreSuffix(fname, newSfx string) string {
+	newSfx = "." + newSfx
+	ext := filepath.Ext(fname) // Extension with dot
+	subFname := strings.TrimSuffix(fname, ext)
+	if isAnyKnownSuffix(ext) {
+		if ext == newSfx {
+			return fname
+		}
+		return subFname + newSfx
+	}
+	sfx := filepath.Ext(subFname)
+	if isAnyKnownSuffix(sfx) {
+		if sfx == newSfx {
+			return fname
+		}
+		return strings.TrimSuffix(subFname, sfx) + newSfx + ext
+	}
+	return subFname + newSfx + ext
 }
