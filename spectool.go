893b01174b9c337982256db89f209fff72c5969a
Eugene Shevchenko <shvgn@protonmail.ch>
2015-06-12 14:14:52 +0300

    Moved flags to init()

--- spectool.go
+++ spectool.go
@@ -15,15 +15,43 @@ import (
 )
 
 const (
-	Version  string = "1.0"
-	Author   string = "Eugene Shevchenko"
-	Email    string = "shvgn@protonmail.ch"
-	URL      string = "https://github.com/shvgn/spectool"
-	Liscence string = "MIT"
+	Version string = "1.0"
+	Author  string = "Eugene Shevchenko"
+	Email   string = "shvgn@protonmail.ch"
+	URL     string = "https://github.com/shvgn/spectool"
+	Licence string = "MIT"
 )
 
-// Global for the verbosity control
-var verboseFlag bool
+var (
+	verboseFlag bool // Verbosity of the output
+
+	keepEvFlag bool // Keep units in electron-volts
+	keepNmFlag bool // Keep units in nanometers
+
+	fromFlag float64 // Cut X from value
+	toFlag   float64 // Cut X up to value
+
+	addNumFlag float64 // Add number
+	subNumFlag float64 // Subtract number
+	mulNumFlag float64 // Multiply by number
+	divNumFlag float64 // Divide by number
+
+	addFlag string // Add spectrum
+	subFlag string // Subtract spectrum
+	mulFlag string // Multiply by spectrum
+	divFlag string // Divide by spectrum
+
+	noiseFlag  bool   // Calculate and subtract noise
+	meanFlag   bool   // Calculate arithmetic mean of spectra
+	smoothFlag string // Smooth spectra
+	statsFlag  bool   // Calculate metainfo
+
+	colXFlag int // Column in ASCII file to read X from
+	colYFlag int // Column in ASCII file to read Y from
+
+	outFmtFlag string // Ouput format
+	outDirFlag string // Ouput directory for resulting spectra
+)
 
 // Message on an arithmetic operation
 func opMessage(op, value string) {
@@ -32,49 +60,49 @@ func opMessage(op, value string) {
 	}
 }
 
-func main() {
-
+func init() {
 	// Modificating flags
 	// X Units
-	nm2EvFlag := flag.Bool("2ev", false, "Keep X in electron-volts")
-	ev2NmFlag := flag.Bool("2nm", false, "Keep X in nanometers")
+	flag.BoolVar(&keepEvFlag, "2ev", false, "Keep X in electron-volts")
+	flag.BoolVar(&keepNmFlag, "2nm", false, "Keep X in nanometers")
 
 	// X cutting options
-	fromFlag := flag.Float64("xfrom", -1.0, "X to start from")
-	toFlag := flag.Float64("xto", -1.0, "X to end with")
+	flag.Float64Var(&fromFlag, "xfrom", -1.0, "X to start from")
+	flag.Float64Var(&toFlag, "xto", -1.0, "X to end with")
 
 	// Spectra arithmetic operations with numbers
-	addNumFlag := flag.Float64("nadd", 0.0, "Add a number ")
-	subNumFlag := flag.Float64("nsub", 0.0, "Subtract a number ")
-	mulNumFlag := flag.Float64("nmul", 1.0, "Multiply by a number ")
-	divNumFlag := flag.Float64("ndiv", 1.0, "Divide by a number ")
+	flag.Float64Var(&addNumFlag, "nadd", 0.0, "Add a number ")
+	flag.Float64Var(&subNumFlag, "nsub", 0.0, "Subtract a number ")
+	flag.Float64Var(&mulNumFlag, "nmul", 1.0, "Multiply by a number ")
+	flag.Float64Var(&divNumFlag, "ndiv", 1.0, "Divide by a number ")
 
 	// Spectra operations with other spectra
-	addFlag := flag.String("spadd", "", "Add spectrum")
-	subFlag := flag.String("spsub", "", "Subtract spectrum")
-	mulFlag := flag.String("spmul", "", "Multiply by spectrum")
-	divFlag := flag.String("spdiv", "", "Divide by spectrum")
+	flag.StringVar(&addFlag, "spadd", "", "Add spectrum")
+	flag.StringVar(&subFlag, "spsub", "", "Subtract spectrum")
+	flag.StringVar(&mulFlag, "spmul", "", "Multiply by spectrum")
+	flag.StringVar(&divFlag, "spdiv", "", "Divide by spectrum")
 
 	// Spectra metadata
-	noiseFlag := flag.Bool("n", false, "Subtract noise")
+	flag.BoolVar(&noiseFlag, "n", false, "Subtract noise")
 
-	meanFlag := flag.Bool("mean", false, "(Not implemented) Mean spectrum from all the passed data")
-	smoothFlag := flag.String("smooth", "",
+	flag.BoolVar(&meanFlag, "mean", false, "(Not implemented) Mean spectrum from all the passed data")
+	flag.StringVar(&smoothFlag, "smooth", "",
 		"[ws,po]\t(Not implemented) Smooth data with optionally specified both window size and polynome order")
-	// pleFlag := flag.String("ple", "", "This is set of wavelength or energy walues: -ple=287.5,288,288.5")
 
 	// Non-modificating flags
-	statsFlag := flag.Bool("s", false, "(Not implemented) Collect statistics on the data")
-	colXFlag := flag.Int("colx", 1, "Set number of the X column in passed data files")
-	colYFlag := flag.Int("coly", 2, "Set number of the Y column in passed data ASCII files")
-	// colsFlag := flag.String("cols", "1,2", "Set numbers of X and Y columns")
-	// inFmtFlag := flag.String("if", "ascii", "ascii|tsv|csv\tFormat of the input file")
-	outFmtFlag := flag.String("of", "ascii", "[ascii|tsv|csv]   Format of the output file")
-	outDirFlag := flag.String("od", "", "Directory for output files. If not specified new files are placed near original ones")
-	// verboseFlag := flag.Bool("v", false, "Verbose the actions")
+	flag.BoolVar(&statsFlag, "s", false, "(Not implemented) Collect statistics on the data")
+	flag.IntVar(&colXFlag, "colx", 1, "Set number of the X column in passed data files")
+	flag.IntVar(&colYFlag, "coly", 2, "Set number of the Y column in passed data ASCII files")
+
+	flag.StringVar(&outFmtFlag, "of", "ascii", "[ascii|tsv|csv]   Format of the output file")
+	flag.StringVar(&outDirFlag, "od", "", "Directory for output files. If not specified new files are placed near original ones")
+
 	flag.BoolVar(&verboseFlag, "v", false, "Verbose the actions")
 
 	flag.Parse()
+}
+
+func main() {
 
 	// Parsing filenames from passed strings. Those are considered to be files
 	// and globs in order to work in both Windows cmd and Unix shells
@@ -83,24 +111,22 @@ func main() {
 	var err error
 	var files []string
 
-	for _, arg := range flag.Args() {
-
-		if sw, err = NewSpecWrapper(arg, *colXFlag, *colYFlag); err != nil {
+	for _, arg := range flag.Args() { // Remaining arguments are filepaths or globs to process
+		if sw, err = NewSpecWrapper(arg, colXFlag, colYFlag); err != nil {
 			if verboseFlag {
-				fmt.Println("Cannot open file", arg, ":",
-					err, "Trying woth glob...")
+				fmt.Println("Cannot open file", arg, ":", err, "Trying with glob...")
 			}
 			if files, err = filepath.Glob(arg); err != nil {
 				if verboseFlag {
-					fmt.Println("Nor filename nor correct glob pattern.")
+					fmt.Println("Nor filename nor correct glob pattern. Skipping.")
 				}
 				fmt.Println(err)
 				continue
 			}
 			for _, f := range files { // arg is a valid glob pattern
-				sw, err = NewSpecWrapper(f, *colXFlag, *colYFlag)
+				sw, err = NewSpecWrapper(f, colXFlag, colYFlag)
 				if err != nil {
-					fmt.Println(f+": skipped file because of parse error:", err)
+					fmt.Println(f+": Parse error:", err, "Skipping.")
 					continue
 				}
 				spData = append(spData, sw)
@@ -112,17 +138,17 @@ func main() {
 
 	// Choosing units for the processing
 	// Forbid using -2ev and -2nm together. FIXME Why?
-	if *nm2EvFlag && *ev2NmFlag {
+	if keepEvFlag && keepNmFlag {
 		log.Fatal("Cannot work on nanometers and electron-volts simultaneously. Sorry.")
 	}
-	modifyUnits := *nm2EvFlag || *ev2NmFlag
+	modifyUnits := keepEvFlag || keepNmFlag
 	var ensureUnitsFunc func(float64) float64
 	var unitsPreSuffix string
 
-	if *nm2EvFlag {
+	if keepEvFlag {
 		ensureUnitsFunc = ensureEv
 		unitsPreSuffix = "ev"
-	} else if *ev2NmFlag {
+	} else if keepNmFlag {
 		ensureUnitsFunc = ensureNm
 		unitsPreSuffix = "nm"
 	} else {
@@ -133,45 +159,45 @@ func main() {
 	}
 
 	// X from and to
-	if *fromFlag >= 0 {
-		*fromFlag = ensureUnitsFunc(*fromFlag)
+	if fromFlag >= 0 {
+		fromFlag = ensureUnitsFunc(fromFlag)
 	}
-	if *toFlag >= 0 {
-		*toFlag = ensureUnitsFunc(*toFlag)
+	if toFlag >= 0 {
+		toFlag = ensureUnitsFunc(toFlag)
 	}
-	if *fromFlag > *toFlag {
-		*fromFlag, *toFlag = *toFlag, *fromFlag
+	if fromFlag > toFlag {
+		fromFlag, toFlag = toFlag, fromFlag
 	}
 
 	// Arithmetics operands
 	var addSpectrum, subSpectrum, mulSpectrum, divSpectrum *SpectrumWrapper
 	// var err error
-	if *addFlag != "" {
-		if addSpectrum, err = NewSpecWrapper(*addFlag); err != nil {
+	if addFlag != "" {
+		if addSpectrum, err = NewSpecWrapper(addFlag); err != nil {
 			log.Fatal(err)
 		}
 		if modifyUnits {
 			addSpectrum.s.ModifyX(ensureUnitsFunc)
 		}
 	}
-	if *subFlag != "" {
-		if subSpectrum, err = NewSpecWrapper(*subFlag); err != nil {
+	if subFlag != "" {
+		if subSpectrum, err = NewSpecWrapper(subFlag); err != nil {
 			log.Fatal(err)
 		}
 		if modifyUnits {
 			subSpectrum.s.ModifyX(ensureUnitsFunc)
 		}
 	}
-	if *mulFlag != "" {
-		if mulSpectrum, err = NewSpecWrapper(*mulFlag); err != nil {
+	if mulFlag != "" {
+		if mulSpectrum, err = NewSpecWrapper(mulFlag); err != nil {
 			log.Fatal(err)
 		}
 		if modifyUnits {
 			mulSpectrum.s.ModifyX(ensureUnitsFunc)
 		}
 	}
-	if *divFlag != "" {
-		if divSpectrum, err = NewSpecWrapper(*divFlag); err != nil {
+	if divFlag != "" {
+		if divSpectrum, err = NewSpecWrapper(divFlag); err != nil {
 			log.Fatal(err)
 		}
 		if modifyUnits {
@@ -187,7 +213,7 @@ func main() {
 		}
 
 		// Subtract the noise from the full-length signal
-		if *noiseFlag {
+		if noiseFlag {
 			n := sw.s.Noise()
 			opMessage("-", fmt.Sprintf("%v (noise)", n))
 			sw.s.ModifyY(func(y float64) float64 { return y - n })
@@ -201,7 +227,7 @@ func main() {
 
 			// Real spectrum X is always assumed to be positive
 			// FIXME Make cut one-sided
-			xl, xr := *fromFlag, *toFlag
+			xl, xr := fromFlag, toFlag
 			if xl > 0 && xr > 0 {
 				opMessage(">", fmt.Sprintf("%v", xl))
 				opMessage("<", fmt.Sprintf("%v", xr))
@@ -209,58 +235,58 @@ func main() {
 			}
 		}
 		// Addition and subtracting of spectra should be done before noise calculation
-		if *addFlag != "" {
+		if addFlag != "" {
 			sw.s.Add(addSpectrum.s)
 			opMessage("+", addSpectrum.fname)
 			sw.AddSpOpSuffix("add", addSpectrum.fname)
 		}
-		if *subFlag != "" {
+		if subFlag != "" {
 			sw.s.Subtract(subSpectrum.s)
 			opMessage("-", subSpectrum.fname)
 			sw.AddSpOpSuffix("sub", subSpectrum.fname)
 		}
 
-		if *mulFlag != "" {
+		if mulFlag != "" {
 			opMessage("*", mulSpectrum.fname)
 			sw.s.Multiply(mulSpectrum.s)
 			sw.AddSpOpSuffix("mul", mulSpectrum.fname)
 		}
-		if *divFlag != "" {
+		if divFlag != "" {
 			sw.s.Divide(divSpectrum.s)
 			opMessage("/", divSpectrum.fname)
 			sw.AddSpOpSuffix("div", divSpectrum.fname)
 		}
 
 		// Arithmetics with numbers
-		if *addNumFlag != 0.0 {
-			opMessage("+", fmt.Sprintf("%v", *addNumFlag))
-			sw.s.ModifyY(func(y float64) float64 { return y + *addNumFlag })
-			sw.AddNumOpSuffix("add", *addNumFlag)
+		if addNumFlag != 0.0 {
+			opMessage("+", fmt.Sprintf("%v", addNumFlag))
+			sw.s.ModifyY(func(y float64) float64 { return y + addNumFlag })
+			sw.AddNumOpSuffix("add", addNumFlag)
 		}
-		if *subNumFlag != 0.0 {
-			opMessage("-", fmt.Sprintf("%v", *subNumFlag))
-			sw.s.ModifyY(func(y float64) float64 { return y - *subNumFlag })
-			sw.AddNumOpSuffix("sub", *subNumFlag)
+		if subNumFlag != 0.0 {
+			opMessage("-", fmt.Sprintf("%v", subNumFlag))
+			sw.s.ModifyY(func(y float64) float64 { return y - subNumFlag })
+			sw.AddNumOpSuffix("sub", subNumFlag)
 		}
-		if *mulNumFlag != 1.0 {
-			opMessage("*", fmt.Sprintf("%v", *mulNumFlag))
-			sw.s.ModifyY(func(y float64) float64 { return y * *mulNumFlag })
-			sw.AddNumOpSuffix("mul", *mulNumFlag)
+		if mulNumFlag != 1.0 {
+			opMessage("*", fmt.Sprintf("%v", mulNumFlag))
+			sw.s.ModifyY(func(y float64) float64 { return y * mulNumFlag })
+			sw.AddNumOpSuffix("mul", mulNumFlag)
 		}
-		if *divNumFlag != 1.0 {
-			opMessage("/", fmt.Sprintf("%v", *divNumFlag))
-			sw.s.ModifyY(func(y float64) float64 { return y / *divNumFlag })
-			sw.AddNumOpSuffix("div", *divNumFlag)
+		if divNumFlag != 1.0 {
+			opMessage("/", fmt.Sprintf("%v", divNumFlag))
+			sw.s.ModifyY(func(y float64) float64 { return y / divNumFlag })
+			sw.AddNumOpSuffix("div", divNumFlag)
 		}
 
-		if *smoothFlag != "" {
+		if smoothFlag != "" {
 			// SMOOTH THEM ALL!!!1
 		}
-		if *meanFlag {
+		if meanFlag {
 			// MEAN THEM ALL
 			sw.AddNumOpSuffix("mean", float64(len(spData)))
 		}
-		if *statsFlag {
+		if statsFlag {
 			// Calculate stats
 			// fmt.Println(stats(sw.s))
 		}
@@ -268,37 +294,37 @@ func main() {
 
 	// Saving
 	// Directory to save in
-	if *outDirFlag != "" {
+	if outDirFlag != "" {
 		var perm os.FileMode = 0755 // FIXME Why use something else?
-		err := os.MkdirAll(*outDirFlag, perm)
+		err := os.MkdirAll(outDirFlag, perm)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	// Output format
-	if *outFmtFlag == "" {
-		*outFmtFlag = "ascii"
+	if outFmtFlag == "" {
+		outFmtFlag = "ascii"
 	}
 
 	for _, sw := range spData {
 		var path string
 		var perm os.FileMode = 0644 // FIXME Why use something else?
 
-		if *outDirFlag != "" {
-			path = filepath.Join(*outDirFlag, sw.fname)
+		if outDirFlag != "" {
+			path = filepath.Join(outDirFlag, sw.fname)
 		} else {
 			path = filepath.Join(sw.dir, sw.fname)
 		}
 
-		err := sw.WriteFile(path, *outFmtFlag, perm)
+		err := sw.WriteFile(path, outFmtFlag, perm)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	// --------------------------------------------------------------------------
-	// Take PLE values into account TODO
+	// TODO Take PLE values into account
 
 	// pleVals := make([]float64, 0)
 
@@ -325,12 +351,4 @@ func main() {
 	// 	}
 	// }
 
-	// --------------------------------------------------------------------------
-
-	// fmt.Println("Number of values to average: ", *averPtr)
-	// fmt.Println("PLE detection values passed: ", *pleSet)
-	// fmt.Println("nm to eV: ", *nm2EvFlag)
-	// fmt.Println("eV to nm: ", *ev2NmFlag)
-	// fmt.Println("PLE detection values parsed: ", pleVals)
-	// fmt.Println("Other cmd arguments: ", flag.Args())
 }
